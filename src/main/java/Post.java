218f96a317f7291a1d2ddc11b3b6bf920ae5d968
Анастасия Сергеевна <kuznecova_18@bk.ru>
2023-09-03 21:18:25 +0300

    First commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,13 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
+
+
